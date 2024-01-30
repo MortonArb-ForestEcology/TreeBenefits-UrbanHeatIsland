95a433e9643910181716b7d6487d57130a98a2d7
Christy Rollinson <crollinson@gmail.com>
2024-01-30 14:03:18 -0600

    Update partial effects scripts
    going to need to think about the best way to show the temperature effect now.  I think what we have is working, but it's not as easy to show as the veg splines are because they're not 0-centered on the y

--- scripts/X5_Analyze_ET_PartialEffects.R
+++ scripts/X5_Analyze_ET_PartialEffects.R
@@ -22,20 +22,20 @@ path.figs <- file.path(path.google, "figures_exploratory")
 dir.create(path.figs, recursive=T, showWarnings=F)
 
 
-biome.pall.all = c("Taiga"= "#2c5c74", 
-                   "Tundra"="#6d8e9d",
-                   "Temperate Broadleaf Forest" = "#7f310f",
-                   "Temperate Conifer Forest" = "#4d1e10",
+biome.pall.all = c("boreal forest/taiga"= "#2c5c74", 
+                   "tundra"="#6d8e9d",
+                   "temperate broadleaf/mixed forest" = "#7f310f",
+                   "temperate coniferous forest" = "#4d1e10",
                    "Temperate Grassland/Savanna" = "#b09c41",
-                   "Montane Grassland/Savanna" = "#a0b8c7",
-                   "Mediterranean" = "#bf772e",
-                   "Desert" = "#c89948",
-                   "Flooded Grassland/Savanna" = "#e0dfa1",
-                   "Tropical Grassland/Savanna" = "#a6b39e",
-                   "Tropical Dry Broadleaf Forest" = "#7a9c64",
-                   "Tropical Conifer Forest" = "#488458",
-                   "Tropical Moist Broadleaf Forest"= "#266240",
-                   "Mangroves" = "#9c8c94")
+                   "montane grassland/savanna" = "#a0b8c7",
+                   "mediterranean" = "#bf772e",
+                   "desert/xeric shrublands" = "#c89948",
+                   "flooded grassland/savanna" = "#e0dfa1",
+                   "tropical grassland/savanna" = "#a6b39e",
+                   "tropical dry broadleaf forest" = "#7a9c64",
+                   "tropical coniferous forest" = "#488458",
+                   "tropical moist broadleaf forest"= "#266240",
+                   "mangroves" = "#9c8c94")
 
 world <- map_data("world")
 
@@ -47,7 +47,18 @@ overwrite=T
 ###########################################
 cityAnalyStats <- read.csv(file.path(path.google, "city_stats_all_ET.csv"))
 # cityAnalyStats[,c("dET.TreeEven", "dET.TreeTargetEven", "dET.TreeTargetBottomUp", "dET.ssp245.2050", "dET.ssp245.2100", "dET.ssp585.2050", "dET.ssp585.2100")] <- cityAnalyStats[,c("modET.TreeEven", "modET.TreeTargetEven", "modET.TreeTargetBottomUp", "modET.ssp245.2050", "modET.ssp245.2100", "modET.ssp585.2050", "modET.ssp585.2100")] - cityAnalyStats$modET.Base
+# cityAnalyStats <- cityAnalyStats[!is.na(cityAnalyStats$ETmodel.R2adj) & cityAnalyStats$ETmodel.R2adj>0,]
 summary(cityAnalyStats)
+
+cityStatsBase <- read.csv(file.path(path.google, "city_stats_all.csv"))
+summary(cityStatsBase)
+
+
+# Add in biome designations for our sanity
+unique(cityStatsBase$biome)
+cityAnalyStats <- merge(cityAnalyStats, cityStatsBase[,c("ISOURBID", "biome")], all.x=T, all.y=F)
+
+unique(cityAnalyStats$biome)
 
 # treeDistCurrent <- read.csv(file.path(path.google, "TreeDistribution_Current.csv"))
 # treeDistGreen <- read.csv(file.path(path.google, "TreeDistribution_Greening-BottomUp.csv"))
@@ -104,11 +115,13 @@ pb <- txtProgressBar(min=0, max=nrow(cityAnalyStats), style=3)
 for(rowCity in 1:nrow(cityAnalyStats)){
   setTxtProgressBar(pb, rowCity)
   
-  # test <- splineTree[[CITY]]
-  if(!overwrite & !is.null(splineTree[[CITY]])) next # If we've done this city, skip it
-  
   CITY=cityAnalyStats$ISOURBID[rowCity]
   # print(CITY)
+  
+  # test <- splineTree[[CITY]]
+  if(!overwrite & !is.null(splineTree[[CITY]])) next # If we've done this city, skip it
+  if(is.na(cityAnalyStats$ETmodel.R2adj[rowCity]) | cityAnalyStats$ETmodel.R2adj[rowCity]<0) next # We don't actually have this city; skip it
+  
   
   
   dfCity <- read.csv(file.path(path.cities, CITY, paste0(CITY, "_CityStats_Pixels.csv")))
@@ -121,6 +134,7 @@ for(rowCity in 1:nrow(cityAnalyStats)){
   if(!dir.exists(file.path(path.et, CITY))) next # This city doesn't have any ET model, so just skip it!
   modETCity <- readRDS(file.path(path.et, CITY, paste0(CITY, "_Model-ET_annual_gam.rds")))
   # modETCity <- readRDS("~/Desktop/CAN15001_Model-ET_annual_gam.rds")
+  summary(modETCity$model)
   
   modETCitySum <- readRDS(file.path(path.et, CITY, paste0(CITY, "_Model-ET_annual_gam-summary.rds")))
   # modETCitySum
@@ -131,24 +145,26 @@ for(rowCity in 1:nrow(cityAnalyStats)){
   # Intercept.Max = max intercept, [ ].MaxI = year/temp in the year with the max intercept
   # Temp.Max = max annual temperature; [].MaxT = year/intercept in the warmest year 
   # dfIntercept <- data.frame(ISOURBID = cityAnalyStats$ISOURBID, Intercept.Mean=NA, Temp.Mean=NA, Intercept.Max=NA, Year.MaxI=NA, Temp.MaxI=NA, Temp.Max=NA, Year.MaxT=NA, Intercept.MaxT=NA)
-  dfIntercept$Intercept.Mean[rowCity] <- mean(modETCitySum$p.coeff)
-  dfIntercept$Temp.Mean[rowCity] <- mean(modETCity$model$LST_Day)
+  intYear <- grepl("year", names(modETCitySum$p.coeff))
+  dfIntercept$Intercept.Mean[rowCity] <- mean(modETCitySum$p.coeff[intYear])
+  dfIntercept$Temp.Mean[rowCity] <- mean(modETCity$model$Tair_f_inst_mean)
   
   # yrstr <- paste(names(modETCitySum$p.coeff)[1])
-  indMaxI <- which(modETCitySum$p.coeff==max(modETCitySum$p.coeff))
-  yrMax <- as.numeric(stringr::str_sub(names(modETCitySum$p.coeff)[indMaxI], start=-4))
-  dfIntercept$Intercept.Max[rowCity] <- modETCitySum$p.coeff[indMaxI]
+  indMaxI <- which(modETCitySum$p.coeff[intYear]==max(modETCitySum$p.coeff[intYear]))
+  yrMax <- as.numeric(stringr::str_sub(names(modETCitySum$p.coeff[intYear])[indMaxI], start=-4))
+  dfIntercept$Intercept.Max[rowCity] <- modETCitySum$p.coeff[intYear][indMaxI]
   dfIntercept$Year.MaxI[rowCity] <- yrMax
   
   
   # The data frame used to fit the model is stored in modETCity$model
-  lstYr <- aggregate( LST_Day ~ `as.factor(year)`, data=modETCity$model, FUN=mean, na.rm=T)
-  dfIntercept$Temp.MaxI[rowCity] <- lstYr$LST_Day[indMaxI]
+  lstYr <- aggregate( Tair_f_inst_mean ~ `as.factor(year)`, data=modETCity$model, FUN=mean, na.rm=T)
+  dfIntercept$Temp.MaxI[rowCity] <- lstYr$Tair_f_inst_mean[lstYr$`as.factor(year)`==yrMax]
   
-  indMaxT <- which(lstYr$LST_Day==max(lstYr$LST_Day))
-  dfIntercept$Temp.Max[rowCity] <- lstYr$LST_Day[indMaxT]
-  dfIntercept$Year.MaxT[rowCity] <- as.numeric(paste(lstYr$`as.factor(year)`[indMaxT]))
-  dfIntercept$Intercept.MaxT[rowCity] <- modETCitySum$p.coeff[indMaxT]
+  indMaxT <- which(lstYr$Tair_f_inst_mean==max(lstYr$Tair_f_inst_mean))
+  yrMaxT <- as.numeric(paste(lstYr$`as.factor(year)`[indMaxT]))
+  dfIntercept$Temp.Max[rowCity] <- lstYr$Tair_f_inst_mean[indMaxT]
+  dfIntercept$Year.MaxT[rowCity] <- yrMaxT
+  dfIntercept$Intercept.MaxT[rowCity] <- modETCitySum$p.coeff[paste0("as.factor(year)", yrMaxT)]
   
   write.csv(dfIntercept, file.path(path.google, "ETModel_InterceptSummary.csv"), row.names=F)
   
@@ -161,30 +177,30 @@ for(rowCity in 1:nrow(cityAnalyStats)){
   # names(modETCity$model)
   treeMean <- mean(modETCity$model$cover.tree)
   vegMean <- mean(modETCity$model$cover.veg)
-  lstMean <- mean(modETCity$model$LST_Day)
+  tempMean <- mean(modETCity$model$Tair_f_inst_mean)
   xMean <- mean(modETCity$model$x)
   yMean <- mean(modETCity$model$y)
   
   # Tree Effect!
-  dfTree <- data.frame(cover.tree = seq(min(modETCity$model$cover.tree), max(modETCity$model$cover.tree), length.out=nInterval), cover.veg=vegMean, LST_Day=lstMean, x=xMean, y=yMean, year=lstYr$`as.factor(year)`[indMaxT])
+  dfTree <- data.frame(cover.tree = seq(min(modETCity$model$cover.tree), max(modETCity$model$cover.tree), length.out=nInterval), cover.veg=vegMean, Tair_f_inst_mean=tempMean, x=xMean, y=yMean, year=lstYr$`as.factor(year)`[indMaxT])
   splineTree[[CITY]] <- data.frame(ISOURBID = CITY, Effect=as.numeric(predict(modETCity, type="terms", terms="s(cover.tree)", newdata=dfTree)), cover.tree=dfTree$cover.tree)
 
   # Non-Tree vegetation
-  dfVeg <- data.frame(cover.veg = seq(min(modETCity$model$cover.veg), max(modETCity$model$cover.veg), length.out=nInterval), cover.tree=treeMean, LST_Day=lstMean, x=xMean, y=yMean, year=lstYr$`as.factor(year)`[indMaxT])
+  dfVeg <- data.frame(cover.veg = seq(min(modETCity$model$cover.veg), max(modETCity$model$cover.veg), length.out=nInterval), cover.tree=treeMean, Tair_f_inst_mean=tempMean, x=xMean, y=yMean, year=lstYr$`as.factor(year)`[indMaxT])
   splineVeg[[CITY]] <- data.frame(ISOURBID = CITY, Effect=as.numeric(predict(modETCity, type="terms", terms="s(cover.veg)", newdata=dfVeg)), cover.veg=dfVeg$cover.veg)
   
   # Temperature
-  dfTemp <- data.frame(cover.tree = treeMean, cover.veg=vegMean, LST_Day=seq(min(modETCity$model$LST_Day), max(modETCity$model$LST_Day), length.out=nInterval), x=xMean, y=yMean, year=lstYr$`as.factor(year)`[indMaxT])
-  splineTemp[[CITY]] <- data.frame(ISOURBID = CITY, Effect=as.numeric(predict(modETCity, type="terms", terms="s(LST_Day)", newdata=dfTemp)), LST_Day=dfTemp$LST_Day)
+  dfTemp <- data.frame(cover.tree = treeMean, cover.veg=vegMean, Tair_f_inst_mean=seq(min(modETCity$model$Tair_f_inst_mean), max(modETCity$model$Tair_f_inst_mean), length.out=nInterval), x=xMean, y=yMean, year=lstYr$`as.factor(year)`[indMaxT])
+  splineTemp[[CITY]] <- data.frame(ISOURBID = CITY, Effect=as.numeric(predict(modETCity, type="terms", terms="Tair_f_inst_mean", newdata=dfTemp)), Tair_f_inst_mean=dfTemp$Tair_f_inst_mean)
   
   
   saveRDS(splineTree, file=file.path(path.google, "ETModel_Spline_PartialEffects_CoverTree.rds"))
   saveRDS(splineVeg, file=file.path(path.google, "ETModel_Spline_PartialEffects_CoverVeg.rds"))
-  saveRDS(splineTemp, file=file.path(path.google, "ETModel_Spline_PartialEffects_LST-Day.rds"))
+  saveRDS(splineTemp, file=file.path(path.google, "ETModel_Spline_PartialEffects_Tair.rds"))
   
   
   # ggplot(data=splineTemp[[CITY]]) +
-  #   geom_line(aes(x=LST_Day, y=Effect))
+  #   geom_line(aes(x=Tair_f_inst_mean, y=Effect))
 
   # ------------------
   
@@ -198,12 +214,12 @@ for(rowCity in 1:nrow(cityAnalyStats)){
 ###########################################
 
 dfSplineTree <- data.table::rbindlist(splineTree)
-dfSplineTree <- merge(dfSplineTree, cityAnalyStats[,c("ISOURBID", "biomeName")], all.x=T, all.y=F)
+dfSplineTree <- merge(dfSplineTree, cityAnalyStats[,c("ISOURBID", "biome")], all.x=T, all.y=F)
 summary(dfSplineTree)
 
 png(file.path(path.figs, "ETmodel_PartialEffects_CoverTree.png"), height=8, width=10, units="in", res=320)
 ggplot(data=dfSplineTree, aes(x=cover.tree, y=Effect)) +
-  facet_wrap(~biomeName) +
+  facet_wrap(~biome) +
   geom_line(aes(group=ISOURBID), linewidth=0.1) + 
   geom_smooth() +
   coord_cartesian(ylim=c(-0.75, 0.75)) +
@@ -211,12 +227,12 @@ ggplot(data=dfSplineTree, aes(x=cover.tree, y=Effect)) +
 dev.off()
 
 dfSplineVeg <- data.table::rbindlist(splineVeg)
-dfSplineVeg <- merge(dfSplineVeg, cityAnalyStats[,c("ISOURBID", "biomeName")], all.x=T, all.y=F)
+dfSplineVeg <- merge(dfSplineVeg, cityAnalyStats[,c("ISOURBID", "biome")], all.x=T, all.y=F)
 summary(dfSplineVeg)
 
 png(file.path(path.figs, "ETmodel_PartialEffects_CoverVeg.png"), height=8, width=10, units="in", res=320)
 ggplot(data=dfSplineVeg, aes(x=cover.veg, y=Effect)) +
-  facet_wrap(~biomeName) +
+  facet_wrap(~biome) +
   geom_line(aes(group=ISOURBID), linewidth=0.1) + 
   geom_smooth() +
   coord_cartesian(ylim=c(-0.75, 0.75)) +
@@ -225,17 +241,17 @@ dev.off()
 
 
 dfSplineTemp <- data.table::rbindlist(splineTemp)
-dfSplineTemp <- merge(dfSplineTemp, cityAnalyStats[,c("ISOURBID", "biomeName")], all.x=T, all.y=F)
+dfSplineTemp <- merge(dfSplineTemp, cityAnalyStats[,c("ISOURBID", "biome")], all.x=T, all.y=F)
 summary(dfSplineTemp)
 
-png(file.path(path.figs, "ETmodel_PartialEffects_LST.png"), height=8, width=10, units="in", res=320)
-ggplot(data=dfSplineTemp[dfSplineTemp$LST_Day>0], aes(x=LST_Day, y=Effect)) +
-  facet_wrap(~biomeName) +
+png(file.path(path.figs, "ETmodel_PartialEffects_Tair.png"), height=8, width=10, units="in", res=320)
+ggplot(data=dfSplineTemp[dfSplineTemp$Tair_f_inst_mean>0], aes(x=Tair_f_inst_mean, y=Effect)) +
+  facet_wrap(~biome) +
   geom_line(aes(group=ISOURBID), linewidth=0.1) + 
-  geom_smooth() +
-  geom_vline(xintercept = 35, color="orange2") +
-  geom_vline(xintercept = 40, color="red2") +
-  coord_cartesian(ylim=c(-0.75, 0.75)) +
+  # geom_smooth() +
+  # geom_vline(xintercept = 35, color="orange2") +
+  # geom_vline(xintercept = 40, color="red2") +
+  # coord_cartesian(ylim=c(-0.75, 0.75)) +
   theme_bw()
 dev.off()
 
