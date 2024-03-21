87d32b05b3f28d0957d0abe72ae5a47176bfd643
Christy Rollinson <crollinson@gmail.com>
2024-03-21 17:25:55 -0500

    add figures for sects 2 & 3
    the code for sect3 won't work yet, but I like the figure I made in script 18, so I copied the bones there

--- scripts/92_Manuscript_Analyses_and_Figures_Sect2-Greening.R
+++ scripts/92_Manuscript_Analyses_and_Figures_Sect2-Greening.R
@@ -4,7 +4,7 @@
 #     Trees have a clear, consistent cooling potential on global urban surface temperatures… [treat cooling capacity as a known; we add a tiny bit of nuance; quickly bring in nuance & water]
 # 02. Cities need more trees to offset UHIs; more trees means more water; ----
 # 03. Warming will increase water demand; precipitation will not keep pace in many regions ----
-library(ggplot2)
+library(ggplot2); library(cowplot)
 
 path.google <- file.path("~/Google Drive/Shared drives/Urban Ecological Drought/Trees-UHI Manuscript/Analysis_v3")
 path.cities <- file.path(path.google, "data_processed_final")
@@ -27,6 +27,27 @@ StatsCombined$biomeCode <- factor(StatsCombined$biomeCode, levels=biome.order$bi
 StatsCombined$biomeNameRev <- factor(StatsCombined$biomeName, levels=rev(levels(StatsCombined$biomeName)))
 StatsCombined$biomeCodeRev <- factor(StatsCombined$biomeCode, levels=rev(levels(StatsCombined$biomeCode)))
 summary(StatsCombined)
+
+cityETStats <- read.csv(file.path(path.google, "city_stats_all_ET_scenarios.csv"))
+summary(cityETStats)
+
+StatsCombined <- merge(StatsCombined, cityETStats[,c("ISOURBID", "tree.mean.TreeTargetBottomUp", "modET.Base", "modET.TreeTargetBottomUp")], all.x=T, all.y=F)
+summary(StatsCombined)
+
+
+# # Currently commented out because I think we'd have to go back and redo the biome targets
+# # NOTE! NOTE! NOTE! NOTE! NOTE! NOTE! NOTE! NOTE! 
+# # Here we're going to JUST look at cities with UHIs
+# # NOTE! NOTE! NOTE! NOTE! NOTE! NOTE! NOTE! NOTE! 
+# citiesUHI <- which(StatsCombined$value.LST.diff>0 & StatsCombined$value.LST.diff.p<0.01)
+# length(citiesUHI)
+# 
+# StatsUHI <- StatsCombined[citiesUHI,]
+# summary(StatsUHI)
+# # NOTE! NOTE! NOTE! NOTE! NOTE! NOTE! NOTE! NOTE! 
+
+
+
 #-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-
 
 
@@ -44,9 +65,189 @@ summary(StatsCombined)
 #       2.2.2. Uniform canopy water estimates; Reaching the same mean canopy level as should have the same effects on LST because we used linear relationship, but will require XX% more water
 #       2.2.3. Benchmark against current precip
 #-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-
-# Do analyses here!
+
+#-#-#-#-#-#-#-#- 
+# Do all necessary calculations up front
+#-#-#-#-#-#-#-#- 
+# Cooling contributed by current tree cover
+# Breaking down contributions of tree cover to LST & UHI ----
+StatsCombined$EffectLST.tree <- StatsCombined$LSTmodel.tree.slope*StatsCombined$value.tree.core # Cooling of trees in core
+StatsCombined$EffectLST.other <- StatsCombined$LSTmodel.veg.slope*StatsCombined$value.other.core # coolign of other veg in core
+StatsCombined$ContribUHI.tree <- StatsCombined$LSTmodel.tree.slope*StatsCombined$value.tree.diff # temp effect of tree diff w/ buffer
+StatsCombined$ContribUHI.other <- StatsCombined$LSTmodel.veg.slope*StatsCombined$value.other.diff # temp effect of other veg diff w/ buffer
+summary(StatsCombined)
 
 
+# How much more trees would there need to be to offset UHI?
+StatsCombined$TreeCoverUHINeed <- -StatsCombined$value.LST.diff/StatsCombined$LSTmodel.tree.slope # How much MORE tree cover you'd need
+StatsCombined$OtherCoverUHINeed <- -StatsCombined$value.LST.diff/StatsCombined$LSTmodel.veg.slope # How much MORE other veg cover you'd need
+StatsCombined$TreeCoverTargetUHI <- StatsCombined$TreeCoverUHINeed + StatsCombined$value.tree.core # Add MORE to CURRENT
+StatsCombined$OtherCoverTargetUHI <- StatsCombined$OtherCoverUHINeed + StatsCombined$value.other.core # Add MORE to CURRENT
+summary(StatsCombined)
+
+# Calculating cooling trees added from bottom-up scenario and associated cooling
+StatsCombined$greening.treeAdd <- StatsCombined$tree.mean.TreeTargetBottomUp - StatsCombined$value.tree.core
+StatsCombined$greening.coolingAdd <- StatsCombined$greening.treeAdd*StatsCombined$LSTmodel.tree.slope
+StatsCombined$greening.treeTotal <- StatsCombined$tree.mean.TreeTargetBottomUp # Yeah, just renaming, but it will help below
+StatsCombined$greening.coolingTotal <- StatsCombined$tree.mean.TreeTargetBottomUp*StatsCombined$LSTmodel.tree.slope
+StatsCombined$greening.remainUHI <- StatsCombined$value.LST.diff + StatsCombined$greening.coolingTotal
+summary(StatsCombined)
+
+# Water Ratios
+StatsCombined$ETcurrent.Precip <- StatsCombined$modET.Base/StatsCombined$Precip.GLDAS
+StatsCombined$ETgreenTarget.Precip <- StatsCombined$modET.TreeTargetBottomUp/StatsCombined$Precip.GLDAS
+summary(StatsCombined)
+
+length(which(StatsCombined$ETcurrent.Precip<1 & !is.na(StatsCombined$ETcurrent.Precip)))
+length(which(StatsCombined$ETgreenTarget.Precip<1 & !is.na(StatsCombined$ETgreenTarget.Precip)))
+# Only ~ 220 cities lose their sustainability from greening!
+#-#-#-#-#-#-#-#- 
+
+
+# Comparing how much the 50% does to get you to the UHI offset target; uses a biome-average
+summary(StatsCombined[,c("value.tree.core", "TreeCoverUHINeed", "greening.treeAdd")]) 
+
+summary((StatsCombined$greening.treeAdd+StatsCombined$value.tree.core)/StatsCombined$value.tree.core) # This would be a ratio of current canopy 
+summary((StatsCombined$TreeCoverUHINeed+StatsCombined$value.tree.core)/StatsCombined$value.tree.core) # This would be a ratio of current canopy 
+
+summary(StatsCombined$greening.treeAdd/StatsCombined$TreeCoverUHINeed) # Note: this is the same as doing it with temperature because the linear relationship & how our goal is set up
+
+
+
+TreeCoverTarget <- ggplot(data=StatsCombined[,], aes(x=biomeCode, y=TreeCoverTargetUHI, fill="Biome Target", color="Biome Target")) +  
+  geom_bar(stat="summary", fun="median") +
+  geom_hline(yintercept=30, linetype="dashed") +
+  # geom_segment(yend=0, aes(xend=biomeCode), stat="summary", fun="median", size=2) +
+  geom_violin(aes(x=biomeCode, y=value.tree.core, fill="Current", color="Current"), scale="width", alpha=0.8) +
+  geom_violin(aes(x=biomeCode, y=tree.mean.TreeTargetBottomUp, fill="50% Greening", color="50% Greening"), scale="width", alpha=0.5) +
+  annotate("text", x=10.5, y=33, label="30%", hjust=1, vjust=0) +
+  # geom_point(stat="summary", fun="median", size=5) +
+  scale_fill_manual(name="Tree Cover", values=c("Current"="#005a32", "Biome Target"=grad.tree[4], "50% Greening"=grad.tree[6])) +
+  scale_color_manual(name="Tree Cover", values=c("Current"="#005a32", "Biome Target"=grad.tree[4], "50% Greening"=grad.tree[6])) +
+  labs(x="Biome", y="Tree Cover (%)") +
+  scale_y_continuous(limits=c(0,70), expand=c(0,0)) +
+  theme_bw()+
+  theme(legend.position="top",
+        legend.title=element_text(face='bold'),
+        panel.background = element_rect(fill=NA),
+        panel.grid=element_blank(),
+        axis.ticks.length = unit(-0.25, "lines"),
+        axis.text.y=element_text(color="black"),
+        axis.title=element_text(color="black", face="bold"),
+        axis.text.x=element_text(color="black", angle=-20, hjust=0),
+        # axis.text.x=element_blank(),
+        # axis.title.x=element_blank(),
+        plot.margin = unit(c(0.5, 0.5, 0.5, 0.5), "lines"))
+
+png(file.path(path.figsExplore, "TreeCover_Targets.png"), height=6, width=6, units="in", res=320)
+TreeCoverTarget
+dev.off()
+
+
+# Now looking at the cooling vs. UHI of the current and our scenario
+# citiesUHI <- which(StatsCombined$value.LST.diff>0 & StatsCombined$value.LST.diff.p<0.01)
+
+effectsUHI <- stack(StatsCombined[,c("EffectLST.tree", "value.LST.diff")])
+names(effectsUHI) <- c("Current", "effect")
+effectsUHI$GreeningTarget <- stack(StatsCombined[,c("greening.coolingTotal", "greening.remainUHI")])[,1]
+effectsUHI$effect <- as.character(effectsUHI$effect)
+effectsUHI$effect[grep("tree", effectsUHI$effect)] <- "Tree"
+effectsUHI$effect[grep("LST", effectsUHI$effect)] <- "Remaining UHI"
+effectsUHI$effect <- factor(effectsUHI$effect, levels=c("Tree", "Remaining UHI"))
+summary(effectsUHI)
+
+effectsUHI[,c("ISOURBID", "biomeName", "biomeCode", "biomeCodeRev")] <- StatsCombined[,c("ISOURBID", "biomeName", "biomeCode", "biomeCodeRev")]
+summary(effectsUHI)
+
+effectsUHI2 <- stack(effectsUHI[,c("Current", "GreeningTarget")])
+names(effectsUHI2)[2] <- c("Scenario")
+effectsUHI2[,c("effect", "ISOURBID", "biomeName", "biomeCode", "biomeCodeRev")] <- effectsUHI[,c("effect", "ISOURBID", "biomeName", "biomeCode", "biomeCodeRev")]
+summary(effectsUHI2)
+
+
+# "#67001f" "#b2182b" "#d6604d" "#f4a582" "#fbbdc7" "#d1e5f0" "#92c5de" "#4393c3" "#2166ac" "#053061"
+# "#f03b20"    "#ef3b2c"
+plotTempEffects <- ggplot(data=effectsUHI2, aes(x=Scenario, y=values, fill=effect)) + 
+  facet_wrap(~biomeCode) +
+  geom_bar(stat="summary", fun="mean", position="dodge") +
+  stat_summary(fun = mean, 
+               fun.min=function(x){mean(x)-sd(x)}, 
+               fun.max=function(x){mean(x)+sd(x)}, 
+               geom="pointrange", position=position_dodge(0.9)) +
+  # stat_summary(fun.data = "mean_cl_normal", conf.int=0.95, geom="pointrange", position=position_dodge(0.9)) +
+  geom_hline(yintercept=0, size=0.5, color="black") +
+  # scale_fill_manual(name="Temp. Source", values=c("Tree"="#005a32", "Non-Tree Veg"=rev(grad.other)[4], "Remaining UHI"="#fb6a4a")) +
+  scale_fill_manual(name="Temp. Source", values=c("Tree"="#005a32", "Non-Tree Veg"=rev(grad.other)[3], "Remaining UHI"=rev(grad.lst)[3])) +
+  scale_color_manual(name="Temp. Source", values=c("Tree"="#005a32", "Non-Tree Veg"=rev(grad.other)[3], "Remaining UHI"=rev(grad.lst)[3])) +
+  geom_text(x="Des", y=-7, hjust=1, label="Cooling Effect") +
+  geom_text(x="Des", y=2.5, hjust=1, label="Warming Effect") +
+  # coord_cartesian(ylim=c(-7.5, 2.5)) +
+  labs(x="Biome", y="Temperature Effect (˚C)") +
+  theme_bw()+
+  theme(legend.position="top",
+        legend.title=element_text(face="bold"),
+        panel.background = element_rect(fill=NA),
+        panel.grid=element_blank(),
+        axis.ticks.length = unit(-0.25, "lines"),
+        axis.text.y=element_text(color="black"),
+        axis.title=element_text(color="black", face="bold"),
+        axis.text.x=element_text(color="black", angle=-20, hjust=0),
+        # axis.text.x=element_blank(),
+        # axis.title.x=element_blank(),
+        plot.margin = unit(c(0.5, 0.5, 0.5, 0.5), "lines"))
+
+png(file.path(path.figsExplore, "TreeCover_Targets-Cooling.png"), height=6, width=6, units="in", res=320)
+plotTempEffects
+dev.off()
+
+
+# Compare current vs. scenario with water use
+plotPrecip <- ggplot(data=StatsCombined[,]) +
+  geom_violin(aes(x=biomeCode, y=Precip.GLDAS, fill=biomeName), scale="width") +
+  scale_fill_manual(values=biome.pall.all) +
+  scale_y_continuous(limits=c(0, max(effectsET$Precip.GLDAS, na.rm=T))) +
+  labs(y="GLDAS Precip (kg/m2/day)", x="Biome") +
+  guides(fill="none") +
+  theme_bw() 
+plotPrecip
+
+effectsET <- stack(StatsCombined[,c("modET.Base", "modET.TreeTargetBottomUp")])
+names(effectsET) <- c("ET", "Scenario")
+effectsET$ETratio <- stack(StatsCombined[,c("ETcurrent.Precip", "ETgreenTarget.Precip")])[,1]
+effectsET$Scenario <- as.character(effectsET$Scenario)
+effectsET$Scenario[grepl("Base", effectsET$Scenario)] <- "Current"
+effectsET$Scenario[grepl("Target", effectsET$Scenario)] <- "GreeningTarget"
+effectsET[,c("ISOURBID", "biomeName", "biomeCode", "biomeCodeRev", "Precip.GLDAS")] <- StatsCombined[,c("ISOURBID", "biomeName", "biomeCode", "biomeCodeRev", "Precip.GLDAS")]
+summary(effectsET)
+
+# "Current"="#005a32", "Biome Target"=grad.tree[4], "50% Greening"=grad.tree[6])
+
+plotET <- ggplot(data=effectsET[,]) +
+  geom_violin(aes(x=biomeCode, y=ET, fill=Scenario), scale="width") +
+  scale_fill_manual(values=c("Current"="#005a32", "GreeningTarget"=grad.tree[6])) +
+  labs(y="ET (kg/m2/day)", x="Biome") +
+  scale_y_continuous(limits=c(0, max(effectsET$Precip.GLDAS, na.rm=T))) +
+  # guides(fill="none") +
+  theme_bw() + theme(plot.background = element_blank())
+
+plotET
+
+plotETratio <- ggplot(data=effectsET[,]) +
+  geom_violin(aes(x=biomeCode, y=log(ETratio), fill=Scenario), scale="width") +
+  scale_fill_manual(values=c("Current"="#005a32", "GreeningTarget"=grad.tree[6])) +
+  geom_hline(yintercept=log(1), linetype="dashed") +
+  annotate(geom="text", x=1.25, y=c(-2.5, 3), label=c("Precip Surplus", "Precip Deficit"), hjust=0) +
+  labs(y="log(ET/Precip) (unitless)", x="Biome") +
+  # guides(fill="none") +
+  theme_bw() + theme(plot.background = element_blank())
+
+plotETratio
+
+# plotETcomb <- cowplot::plot_grid(plotET,NA, plotETratio, NA, ncol=2, rel_widths=c(0.75, 0.25))
+
+png(file.path(path.figsExplore, "TreeCover_Targets-ET.png"), height=6, width=8, units="in", res=320)
+cowplot::plot_grid(plotPrecip, plotET,plotETratio, ncol=1, rel_heights=c(0.4, 0.3, 0.3))
+dev.off()
 
 #-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-
 
--- scripts/93_Manuscript_Analyses_and_Figures_Sect3-Warming.R
+++ scripts/93_Manuscript_Analyses_and_Figures_Sect3-Warming.R
@@ -4,7 +4,7 @@
 #     Trees have a clear, consistent cooling potential on global urban surface temperatures… [treat cooling capacity as a known; we add a tiny bit of nuance; quickly bring in nuance & water]
 # 02. Cities need more trees to offset UHIs; more trees means more water; ----
 # 03. Warming will increase water demand; precipitation will not keep pace in many regions ----
-library(ggplot2)
+library(ggplot2); library(cowplot)
 
 path.google <- file.path("~/Google Drive/Shared drives/Urban Ecological Drought/Trees-UHI Manuscript/Analysis_v3")
 path.cities <- file.path(path.google, "data_processed_final")
@@ -28,6 +28,37 @@ StatsCombined$biomeNameRev <- factor(StatsCombined$biomeName, levels=rev(levels(
 StatsCombined$biomeCodeRev <- factor(StatsCombined$biomeCode, levels=rev(levels(StatsCombined$biomeCode)))
 summary(StatsCombined)
 
+
+# Creating a combined ET data frame
+# NOTE that for the cmip6 scenarios, we added the change in temp to the gldas air temp; so lets create a precip correction
+cmip6 <- read.csv(file.path(path.google, "city_stats_all_CMIP6_ET.csv"))
+cmip6$Scenario <- as.factor(cmip6$Scenario)
+cmip6$Time <- as.factor(cmip6$Time)
+cmip6 <- merge(cmip6, cityAnalyStats[,c("ISOURBID", "biomeName", "biomeCode", "modET.Base")], all.x=T, all.y=F)
+cmip6$modET.diff <- cmip6$modET - cmip6$modET.Base
+summary(cmip6)
+
+cmip6 <- merge(cmip6, datGLDAS, all.x=T, all.y=F)
+cmip6$pr.adj <- cmip6$pr.gldas*cmip6$pr.per # Creating an adjusted daily precip by looking at the % change in pr for each run
+summary(cmip6)
+
+
+# Looking at the ET vs Precip Ratio --> NOTE: Using adjusted PR because we adjusted ET
+cmip6$modET.Precip <- cmip6$modET/cmip6$pr.adj
+cmip6$modET.Precip[cmip6$modET.Precip==Inf] <- NA
+# cmip6$modET.PrecipLog <- log(cmip6$modET.Precip)
+summary(cmip6)
+
+cmip6AggMean <- aggregate(cbind(tas.diff, pr.diff, pr.per, modET, modET.Base, modET.diff, modET.Precip)~ISOURBID + LATITUDE + LONGITUDE + biomeName + biomeCode + Scenario + Time, data=cmip6, FUN=mean, na.rm=T)
+cmip6AggMean$biomeName <- factor(cmip6AggMean$biomeName, levels=biome.order$biomeName)
+summary(cmip6AggMean)
+
+etSummary <- rbind(cityAnalyStats[,c("ISOURBID", "LATITUDE", "LONGITUDE", "biomeName", "biomeCode", "Scenario", "modET.Precip")],
+                   cmip6AggMean[cmip6AggMean$Time=="2100",c("ISOURBID", "LATITUDE", "LONGITUDE", "biomeName", "biomeCode", "Scenario", "modET.Precip")])
+etSummary$biomeName <- factor(etSummary$biomeName, levels=biome.order$biomeName)
+etSummary$biomeCode <- factor(etSummary$biomeCode, levels=rev(biome.order$biomeCode))
+
+summary(etSummary)
 #-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-
 
 
@@ -43,4 +74,46 @@ summary(StatsCombined)
 #-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-
 # Do analyses here!
 
+map.ETratio.All <- ggplot(data=etSummary[,]) +
+  facet_grid(Scenario~. ) +
+  geom_rect(xmin=min(world$long), xmax=max(world$long), ymin=min(world$lat), ymax=max(world$lat), fill="gray80") +
+  geom_map(map=world, data=world, aes( map_id = region), fill="gray30", linewidth=0.1) +
+  # coord_map("merc") +
+  coord_map("moll") +
+  expand_limits(x = world$long, y = world$lat) +
+  geom_point(aes(x=LONGITUDE, y=LATITUDE, color=log(modET.Precip)), size=0.1, alpha=0.8) +
+  scale_color_stepsn(name="log(ET/precip)", colors=rev(grad.prcp), limits=c(-3, 3), n.breaks=13, oob=squish) + # Using breaks from IPCC AR6 figures
+  theme(legend.position="left",
+        legend.title=element_text(color="black", face="bold"),
+        legend.text=element_text(color="black"),
+        legend.background=element_blank(),
+        legend.key.width = unit(2, "lines"),
+        legend.key.height = unit(2, "lines"),
+        # legend.key.height = unit(1.5, "lines"),
+        axis.ticks=element_blank(),
+        axis.text=element_blank(),
+        axis.title=element_blank(),
+        panel.background = element_rect(fill="NA"),
+        panel.grid = element_blank(), 
+        plot.margin=margin(0.5,0.5, 2, 0.5, "lines"))
+map.ETratio.All
+
+plotRatioLog <- ggplot(data=etSummary[!is.na(etSummary$biomeName),]) +
+  facet_grid(Scenario~.) +
+  # coord_flip() +
+  # coord_cartesian(ylim=c(0,2.5), expand=0) +
+  geom_violin(aes(y=biomeCode, x=log(modET.Precip), fill=biomeName), scale="width") +
+  geom_vline(xintercept=0, linetype="dashed") +
+  annotate(geom="text", y=14, x=-3, label=c("Precip Surplus"), hjust=0, size=3) +
+  annotate(geom="text", y=14, x=3, label=c("Precip Deficit"), hjust=0, size=3) +
+  # annotate(geom="text", x=14, y=5, l abel=c("Precip Deficit"), hjust=1) +
+  scale_fill_manual(values=biome.pall.all) +
+  labs(x="log(ET/Precip)", y="Biome") +
+  guides(fill="none") +
+  theme_bw()
+plotRatioLog
+
+png(file.path(path.figs, "ETmodel_ET_vs_Precip_Now-CMIP6_Log_Combined.png"), height=8, width=14, units="in", res=320)
+cowplot::plot_grid(map.ETratio.All, plotRatioLog, ncol=2, rel_widths = c(0.55, 0.45))
+dev.off()
 #-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-
