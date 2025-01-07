9e483ab83c8034c7213b59a5ef5051bf7c70cde9
Christy Rollinson <crollinson@mortonarb.org>
2025-01-07 15:22:25 -0600

    add check of models with no veg cover

--- scripts/02.2_Analysis_ModelCities-ModelSelection.R
+++ scripts/02.2_Analysis_ModelCities-ModelSelection.R
@@ -18,18 +18,24 @@ cityStatsRegion <- read.csv(file.cityStatsRegion)
 # Looking at dR2adj --> Pos is alternate does better
 cityStatsRegion$dR2adj.Log <- cityStatsRegion$LSTmodelLog.R2adj - cityStatsRegion$LSTmodel.R2adj 
 cityStatsRegion$dR2adj.SCover <- cityStatsRegion$LSTmodelSCover.R2adj - cityStatsRegion$LSTmodel.R2adj 
+cityStatsRegion$dR2adj.SCoverTree <- cityStatsRegion$LSTmodelSCoverTree.R2adj - cityStatsRegion$LSTmodel.R2adj 
 
 # change in RMSE; negative means alternate does better
 cityStatsRegion$dRMSE.Log <-cityStatsRegion$LSTmodelLog.RMSE -  cityStatsRegion$LSTmodel.RMSE
 cityStatsRegion$dRMSE.SCover <- cityStatsRegion$LSTmodelSCover.RMSE - cityStatsRegion$LSTmodel.RMSE
+cityStatsRegion$dRMSE.SCoverTree <- cityStatsRegion$LSTmodelSCoverTree.RMSE - cityStatsRegion$LSTmodel.RMSE
 
 # Change in AIC; negative means reduction
 cityStatsRegion$dAIC.Log <- cityStatsRegion$LSTmodelLog.AIC - cityStatsRegion$LSTmodel.AIC
 cityStatsRegion$dAIC.SCover <-  cityStatsRegion$LSTmodelSCover.AIC - cityStatsRegion$LSTmodel.AIC
+cityStatsRegion$dAIC.SCoverTree <-  cityStatsRegion$LSTmodelSCoverTree.AIC - cityStatsRegion$LSTmodel.AIC
+cityStatsRegion$dAIC.SCoverTree2 <-  cityStatsRegion$LSTmodelSCoverTree.AIC - cityStatsRegion$LSTmodelSCover.AIC
 
 # Looking at dRMSE as a percent reduction
 cityStatsRegion$dRMSEperc.Log <- cityStatsRegion$dRMSE.Log/cityStatsRegion$LSTmodel.RMSE
 
 cityStatsRegion$dRMSEperc.SCover <- cityStatsRegion$dRMSE.SCover/cityStatsRegion$LSTmodel.RMSE
+cityStatsRegion$dRMSEperc.SCoverTree <- cityStatsRegion$dRMSE.SCoverTree/cityStatsRegion$LSTmodel.RMSE
 
 summary(cityStatsRegion)
+
